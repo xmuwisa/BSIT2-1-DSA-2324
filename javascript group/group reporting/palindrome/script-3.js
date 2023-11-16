cee8876bb1c22b4c9576b55119a1f9b6c287f45a
Luisa Eustaquio <95476379+xmuwisa@users.noreply.github.com>
2023-11-17 01:23:22 +0800

    Added palindrome version 3

---
+++ javascript group/group reporting/palindrome/script-3.js
@@ -0,0 +1,15 @@
+
+
+
+
+// algorithm IsPalindrome(value)
+// Pre: value 6= ∅ 3)
+// Post: value is determined to be a palindrome or not 
+// word ← value.Strip().ToUpperCase() 
+// left ← 0 
+// right ← word.Length −1
+// while word[left] = word[right] and left < right 
+// left ← left + 1 9) right ← right − 1
+// end while
+// return word[left] = word[right]
+// end IsPalindrome 
