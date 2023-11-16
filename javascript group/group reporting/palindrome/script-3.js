ed9508615e58f23cce8cee7f09c18fc39e6a8295
Luisa Eustaquio <95476379+xmuwisa@users.noreply.github.com>
2023-11-17 01:50:16 +0800

    Revised palindrome version 3

--- javascript group/group reporting/palindrome/script-3.js
+++ javascript group/group reporting/palindrome/script-3.js
@@ -5,17 +5,15 @@ function isPalindrome(input){
     const word = input.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
     //matches any character that is not an uppercase letter (A-Z), a lowercase letter (a-z), or a digit (0-9)
 
-    c_inp.innerHTML = c_input;
-    //display in 'Cleaned:' container the cleaned input
+    let left = 0;
+    let right = word.length - 1;
 
-    const input_len = c_input.length;
-    //get final input's length
+    while (word[left] === word[right] && left < right) {
+        left++;
+        right--;
+    }
 
-    const r_input = reverse_input(c_input, input_len);
-    //call reverse_input function to create the reversed version of the input
-
-    r_inp.innerHTML = r_input;
-    //display in 'Reversed:' container the reversed input
+    return word[left] === word[right];
 }
 
 function enter(){ //the function called when 'Check' button is clicked
@@ -32,46 +30,3 @@ function enter(){ //the function called when 'Check' button is clicked
     //call palindrome check to check if the input is a palindrome or not
     //arguments: final input, input length
 }
-
-
-// function isPalindrome(value) {
-//     // Pre: value is not empty
-//     // Post: value is determined to be a palindrome or not
-
-//     // Strip whitespaces and convert to uppercase
-//     const word = value.trim().toUpperCase();
-
-//     let left = 0;
-//     let right = word.length - 1;
-
-//     // Check if the characters at the corresponding positions are equal
-//     while (word[left] === word[right] && left < right) {
-//         left++;
-//         right--;
-//     }
-
-//     // If the loop completed without breaking, it's a palindrome
-//     return word[left] === word[right];
-// }
-
-// // Example usage:
-// const inputString = "A man a plan a canal Panama";
-// const result = isPalindrome(inputString);
-
-// if (result) {
-//     console.log(`${inputString} is a palindrome.`);
-// } else {
-//     console.log(`${inputString} is not a palindrome.`);
-// }
-
-// algorithm IsPalindrome(value)
-// Pre: value 6= ∅ 3)
-// Post: value is determined to be a palindrome or not 
-// word ← value.Strip().ToUpperCase() 
-// left ← 0 
-// right ← word.Length −1
-// while word[left] = word[right] and left < right 
-// left ← left + 1 9) right ← right − 1
-// end while
-// return word[left] = word[right]
-// end IsPalindrome 
